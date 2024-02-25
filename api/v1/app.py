a923fbe8f4b0226df7b4b497450febd18978883b
root <root@avellin.localdomain>
2024-02-25 17:45:35 +0200

    updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ from models import storage
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
