6308605e8b65eb55b509778b191f4b986ab44649
LoveCode20 <amysweat20@gmail.com>
2024-02-27 04:07:28 +0000

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,22 @@
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
+
+app = Flask(__name__)
+
+@app.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    counts = {}
+    counts['objects'] = {}
+    counts['object']['Object1'] = storage.count('Object1')
+    counts['object']['Object2'] = storage.count('Object2')
+    counts['object']['Object3'] = storage.count('Object3')
+    return jsonify(counts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
